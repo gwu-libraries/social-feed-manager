40308bdf67e5f62e0f66b0ad346e2c9981e1b3f9
Daniel Chudnov <dchud@umich.edu>
2012-03-06 23:47:35 -0500

    more prettying up via bootstrap

--- sfm/ui/views.py
+++ sfm/ui/views.py
@@ -1,9 +1,14 @@
+from django.db.models import Count
 from django.shortcuts import render
 
 from ui.models import Status
 
 def home(request):
+    rules = Status.objects.values('rule_match').annotate(Count('rule_match')).order_by('-rule_match__count')
+    tags = Status.objects.values('rule_tag').annotate(Count('rule_tag')).order_by('-rule_tag__count')
     return render(request, 'home.html', {
         'title': 'home',
+        'rules': rules,
+        'tags': tags,
         'statuses': Status.objects.all(),
         })
--- sfm/urls.py
+++ sfm/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls.defaults import patterns, include, url
 from django.contrib import admin
 from django.views.generic import list_detail
+from django.views.generic import TemplateView
 
 from sfm.ui.models import Status
 
@@ -19,6 +20,8 @@ status_info = {
 
 urlpatterns += patterns('ui.views',
     url(r'^$', 'home', name='home'),
+    url(r'^about/$', TemplateView.as_view(template_name='about.html'),
+        name='about'),
     url(r'^status/page/(?P<page>[0-9]+)/$', list_detail.object_list, 
         status_info, name='status_list'),
 )
