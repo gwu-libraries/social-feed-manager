d191ea2fdd3f89ce04964e3c6eca863c229fa4a6
Daniel Chudnov <dchud@gwu.edu>
2012-09-03 13:45:34 -0400

    removed some status/rule/search stuff, refs #18, #19

--- sfm/sfm/urls.py
+++ sfm/sfm/urls.py
@@ -1,9 +1,7 @@
 from django.conf.urls.defaults import patterns, include, url
 from django.contrib import admin
-from django.views.generic import list_detail
 from django.views.generic import TemplateView
 
-from ui.models import Status
 from ui.views import StatusRuleListView
 
 admin.autodiscover()
@@ -14,19 +12,11 @@ urlpatterns = patterns('',
     url(r'^login/$', 'django.contrib.auth.views.login', name='login'),
 )
 
-status_info = {
-    'queryset': Status.objects.all(),
-    'paginate_by': 50,
-}
-
 urlpatterns += patterns('ui.views',
     url(r'^$', 'home', name='home'),
     url(r'^logout/$', 'logout', name='logout'),
     url(r'^about/$', TemplateView.as_view(template_name='about.html'),
         name='about'),
-    url(r'^search/$', 'search', name='search'),
-    url(r'^status/page/(?P<page>[0-9]+)/$', list_detail.object_list, 
-        status_info, name='status_list'),
     url(r'^trends/weekly/$', 'trends_weekly', name='trends_weekly'),
     url(r'^trends/daily/$', 'trends_daily', name='trends_daily'),
 )
--- sfm/ui/views.py
+++ sfm/ui/views.py
@@ -1,9 +1,7 @@
 from django.contrib import auth
-from django.core.paginator import Paginator, InvalidPage, EmptyPage
 from django.core.urlresolvers import reverse
 from django.db.models import Count
 from django.shortcuts import render, redirect
-from django.views.generic import ListView
 
 from ui.models import Status, TrendWeekly, TrendDaily
 
@@ -16,53 +14,6 @@ def home(request):
         'tags': tags,
         'statuses': Status.objects.all(),
         })
-
-def search(request):
-    q = request.GET.get('q', '')
-    if q:
-        qs_results = Status.objects.filter(summary__icontains=q)
-        paginator = Paginator(qs_results, 50)
-        try:
-            page = int(request.GET.get('page', 1))
-        except ValueError:
-            page = 1
-        try:
-            results = paginator.page(page)
-        except (EmptyPage, InvalidPage):
-            results = paginator.page(paginator.num_pages)
-        return render(request, 'search.html', {
-            'title': 'search "%s"' % q,
-            'q': q,
-            'paginator': paginator,
-            'page_obj': results,
-            'object_list': results.object_list,
-            })
-    return redirect(reverse('home'))
-        
-class StatusRuleListView(ListView):
-
-    template_name = 'ui/status_by_rule_list.html'
-
-    def get_queryset(self):
-        return Status.objects.filter(rule_match__iexact=self.kwargs['rule'])
-
-    def get_context_data(self, **kwargs):
-        context = super(StatusRuleListView, self).get_context_data(**kwargs)
-        qs = self.get_queryset()
-        paginator = Paginator(qs, 50)
-        try: 
-            page = int(self.kwargs.get('page', 1))
-        except ValueError:
-            page = 1
-        try:
-            results = paginator.page(page)
-        except (EmptyPage, InvalidPage):
-            results = paginator.page(paginator.num_pages)
-        context['rule'] = self.kwargs['rule']
-        context['paginator'] = paginator
-        context['page_obj'] = results
-        context['object_list'] = results.object_list
-        return context
 
 def trends_weekly(request):
     trends = TrendWeekly.objects.all()  
