f9a0a27fcf45abc4ec765504ab23038b51744e4d
Daniel Chudnov <dchud@gwu.edu>
2012-11-06 15:55:30 -0500

    tweaked filterstream to use tweepy streamlistener

--- sfm/ui/management/commands/filterstream.py
+++ sfm/ui/management/commands/filterstream.py
@@ -10,13 +10,16 @@ from tweepy.streaming import StreamListener
 
 from ui.models import RotatingFile, Rule
 
-class RotatingFileListener(StreamListener):
 
-    def __init__(self, rf):
-        self._rf = rf
-
+class StdOutListener(StreamListener):
+    
     def on_data(self, data):
-        pass
+        print data
+        return True
+
+    def on_error(self, status):
+        print status
+
 
 class Command(BaseCommand):
     help = 'Filter tweets based on active rules'
@@ -24,6 +27,8 @@ class Command(BaseCommand):
     option_list = BaseCommand.option_list + (
         make_option('--save', action='store_true', default=False,
             dest='save', help='save the data to disk'),
+        make_option('--verbose', action='store_true', default=False,
+            dest='verbose', help='print debugging info to stdout'),
         make_option('--dir', action='store', type='string',
             default=settings.DATA_DIR, dest='dir',
             help='directory for storing the data (default=%s)' % settings.DATA_DIR),
@@ -35,31 +40,37 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         rules = Rule.objects.filter(is_active=True)
         if not rules:
-            print 'no rules to filter on'
+            if options.get('verbose', False):
+                print 'no rules to filter on'
             return
         words = set()
         people = set()
         locations = set()
         for rule in rules:
-            words.update(rule.words.split(' ') if rule.words else [])
-            people.update(rule.people.split(' ') if rule.people else [])
-            locations.update(rule.locations.split(' ') if rule.locations else [])
+            words.update(rule.words.strip().split(' ') if rule.words else [])
+            people.update(rule.people.strip().split(' ') if rule.people else [])
+            locations.update(rule.locations.strip().split(' ') if rule.locations else [])
+        if options.get('verbose', False):
+            print 'track:', words
+            print 'follow:', people
+            print 'locations:', locations
+
         try:
-            gelmanbot = User.objects.get(username='gelmanbot')
-            sa = gelmanbot.social_auth.all()[0]
+            user = User.objects.get(username=settings.TWITTER_DEFAULT_USERNAME)
+            sa = user.social_auth.all()[0]
             auth = tweepy.OAuthHandler(settings.TWITTER_CONSUMER_KEY, 
                     settings.TWITTER_CONSUMER_SECRET)
             auth.set_access_token(sa.tokens['oauth_token'], 
                     sa.tokens['oauth_token_secret'])
-            listener = RotatingFile(filename_prefix='filter',
-                    save_interval_seconds=options['interval'],
-                    data_dir=options['dir'])
-            stream = tweepy.Stream(auth, listener)
-            stream.filter(track=words)
             if options.get('save', False):
-                for line in stream:
-                    if line:
-                        print line
+                listener = StdOutListener()
+            else:
+                listener = RotatingFile(filename_prefix='filter',
+                        save_interval_seconds=options['interval'],
+                        data_dir=options['dir'])
+            stream = tweepy.Stream(auth, listener)
+            stream.filter(track=words, follow=people, locations=locations)
         except Exception, e:
-            print 'Disconnected from twitter:', e
-            print traceback.print_exc()
+            if options.get('verbose', False):
+                print 'Disconnected from twitter:', e
+                print traceback.print_exc()
