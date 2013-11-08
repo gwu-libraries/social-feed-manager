88f753f5ccd9196ae18f373ea472f60985baa1c3
kerchner <kerchner@gwu.edu>
2013-11-08 11:05:50 -0500

    removed duplicate set_wait_time

--- sfm/ui/management/commands/populate_uids.py
+++ sfm/ui/management/commands/populate_uids.py
@@ -12,7 +12,6 @@
 #   https://dev.twitter.com/docs/error-codes-responses
 #   https://dev.twitter.com/docs/rate-limiting
 
-import time
 from optparse import make_option
 
 from django.conf import settings
@@ -22,31 +21,6 @@ import tweepy
 
 from ui.models import authenticated_api
 from ui.models import TwitterUser
-
-# A little added cushion
-WAIT_BUFFER_SECONDS = 2
-
-
-def set_wait_time(last_response):
-    """based on last tweepy api response, calculate a time buffer in
-    seconds to wait before issuing next api call."""
-    wait_time = 0
-    try:
-        remaining = int(last_response.getheader('x-rate-limit-remaining'))
-        reset = int(last_response.getheader('x-rate-limit-reset'))
-        reset_seconds = reset - int(time.time())
-    except:
-        remaining = reset_seconds = 1
-    # the out-of-calls-for-this-window case
-    if remaining == 0:
-        return reset_seconds + WAIT_BUFFER_SECONDS
-    else:
-        wait_time = (reset_seconds / remaining) + WAIT_BUFFER_SECONDS
-    # #22: saw some negative ratelimit-reset/wait_times
-    # so cushion around that too
-    while wait_time < WAIT_BUFFER_SECONDS:
-        wait_time += WAIT_BUFFER_SECONDS
-    return wait_time
 
 
 class Command(BaseCommand):
