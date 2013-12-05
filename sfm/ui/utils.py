d52872e1f55bd549f65c5ee0b0f3bc745bdd1e9c
kerchner <kerchner@gwu.edu>
2013-12-05 16:36:23 -0500

    Cleaned up comments per @dchud direction in https://github.com/gwu-libraries/social-feed-manager/pull/79

--- sfm/ui/utils.py
+++ sfm/ui/utils.py
@@ -1,16 +1,3 @@
-# Cycle through all the TwitterUsers we're tracking and fetch as many
-# new items as possible. Attempt to backfill up to the limit twitter
-# provides (currently 3200 statuses).  Obey timeline and rate limit laws
-# like a good citizen.  For more info:
-#
-# see https://dev.twitter.com/docs/api/1.1/get/statuses/user_timeline
-#   for explanation of user_timeline call
-# see https://dev.twitter.com/docs/working-with-timelines
-#   for explanation of max_id, since_id usage
-# see also:
-#   https://dev.twitter.com/docs/error-codes-responses
-#   https://dev.twitter.com/docs/rate-limiting
-
 import time
 import tweepy
 from django.conf import settings
@@ -43,30 +30,29 @@ def set_wait_time(last_response):
     return wait_time
 
 
-# For a TwitterUser, populate its uid based on its stored screen name,
-# if uid==0 (default value, indicating it hasn't been set yet).
-# if force==True, do it even if uid isn't 0
-# Only do this for active users.
-#
-# see https://dev.twitter.com/docs/api/1.1/get/users/lookup
-#   for explanation of get_user call
-# see https://dev.twitter.com/docs/working-with-timelines
-#   for explanation of max_id, since_id usage
-# see also:
-#   https://dev.twitter.com/docs/error-codes-responses
-#   https://dev.twitter.com/docs/rate-limiting
-
-
 def populate_uid(name, force=False):
-    #TODO: if user is None:
+    """
+    For a TwitterUser, populate its uid based on its stored screen name,
+    if uid==0 (default value, indicating it hasn't been set yet).
+    if force==True, do it even if uid isn't 0
+    Only do this for active users.
+
+    see https://dev.twitter.com/docs/api/1.1/get/users/lookup
+      for explanation of get_user call
+    see https://dev.twitter.com/docs/working-with-timelines
+      for explanation of max_id, since_id usage
+    see also:
+      https://dev.twitter.com/docs/error-codes-responses
+      https://dev.twitter.com/docs/rate-limiting
+    """
 
     api = authenticated_api(username=settings.TWITTER_DEFAULT_USERNAME)
     qs_tweeps = TwitterUser.objects.filter(is_active=True, name=name)
-    #TODO: What if we didn't find someone with that name?
+
     for tweep in qs_tweeps:
         if tweep.uid == 0 or force is True:
             try:
-                #TODO: better way to catch when user isn't found
+                #TODO: better way to catch when user isn't found?
                 user_status = api.get_user(screen_name=name)
                 tweep.uid = user_status['id']
                 tweep.save()
