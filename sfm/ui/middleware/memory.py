416bb713f30202d82f2fa810c8208f50212fafbf
kerchner <kerchner@gwu.edu>
2013-12-26 17:46:49 -0500

    Added middleware for logging memory usage (was in separate Memory_middleware repo)

---
+++ sfm/ui/middleware/memory.py
@@ -0,0 +1,20 @@
+import os
+import psutil
+import sys
+import warnings
+
+THRESHOLD = 2*1024*1024
+
+
+class MemoryUsageMiddleware(object):
+
+    def process_request(self, request):
+        request._mem = psutil.Process(os.getpid()).get_memory_info()
+
+    def process_response(self, request, response):
+        mem = psutil.Process(os.getpid()).get_memory_info()
+        diff = mem.rss - request._mem.rss
+        if diff > THRESHOLD:
+            #print >> sys.stderr, 'MEMORY USAGE = %r KB' % ((diff/1024, request.path),)
+            warnings.warn('MEMORY USAGE = %r KB, path = %s' % (diff/1024, request.path))
+        return response
