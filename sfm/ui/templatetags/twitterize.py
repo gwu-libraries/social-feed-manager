eeb131b7e62f025d89b1a806c575cd60ea7ce3aa
Daniel Chudnov <dchud@gwu.edu>
2014-01-20 18:13:03 -0500

    moved imports to top, changed twitter links to https, refs #100

--- sfm/ui/templatetags/twitterize.py
+++ sfm/ui/templatetags/twitterize.py
@@ -1,5 +1,8 @@
+import re
+
 from django import template
 from django.template.defaultfilters import stringfilter
+from django.utils.html import urlize
 from django.utils.safestring import mark_safe
 
 register = template.Library()
@@ -8,16 +11,14 @@ register = template.Library()
 @register.filter(name='twitterize')
 @stringfilter
 def twitterize(value, autoescape=None):
-    from django.utils.html import urlize
-    import re
     # Link URLs
     value = urlize(value, nofollow=False, autoescape=autoescape)
     # Link twitter usernames prefixed with @
     value = re.sub(r'(\s+|\A)@([a-zA-Z0-9\-_]*)\b',
-                   r'\1<a href="http://twitter.com/\2">@\2</a>', value)
+                   r'\1<a href="https://twitter.com/\2">@\2</a>', value)
     # Link hash tags
     value = re.sub(r'(\s+|\A)#([a-zA-Z0-9\-_]*)\b',
-                   r'\1<a href="http://twitter.com/search?q=%23\2">#\2</a>',
+                   r'\1<a href="https://twitter.com/search?q=%23\2">#\2</a>',
                    value)
     return mark_safe(value)
 twitterize.is_safe = True
