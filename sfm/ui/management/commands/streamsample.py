8637fb2bff43cbd3b3fc2c52afb4fc1564be0357
Ankushi Sharma <anksharm@gwmail.gwu.edu>
2014-02-14 14:05:30 -0500

    streamsample updated with PEP8 changes

--- sfm/ui/management/commands/streamsample.py
+++ sfm/ui/management/commands/streamsample.py
@@ -9,6 +9,7 @@ from tweepy.streaming import StreamListener
 
 from ui.models import RotatingFile
 
+
 class StdOutListener(StreamListener):
 
     def on_data(self, data):
@@ -18,32 +19,38 @@ class StdOutListener(StreamListener):
     def on_error(self, status):
         print status
 
+
 class Command(BaseCommand):
     help = 'Show or save the Twitter sample/spritzer feed'
 
     option_list = BaseCommand.option_list + (
         make_option('--save', action='store_true', default=False,
-            dest='save', help='save the data to disk'),
+                    dest='save', help='save the data to disk'),
         make_option('--dir', action='store', type='string',
-            default=settings.DATA_DIR, dest='dir',
-            help='directory for storing the data (default=%s)' % settings.DATA_DIR),
+                    default=settings.DATA_DIR, dest='dir',
+                    help='directory for storing the data (default=%s)'
+                    % settings.DATA_DIR),
         make_option('--interval', action='store', type='int',
-            default=settings.SAVE_INTERVAL_SECONDS, dest='interval',
-            help='how often to save data (default=%s)' % settings.SAVE_INTERVAL_SECONDS),
+                    default=settings.SAVE_INTERVAL_SECONDS, dest='interval',
+                    help='how often to save data (default=%s)'
+                    % settings.SAVE_INTERVAL_SECONDS),
         )
 
     def handle(self, *args, **options):
         user = User.objects.get(username=settings.TWITTER_DEFAULT_USERNAME)
         sa = user.social_auth.all()[0]
-        auth = tweepy.OAuthHandler(settings.TWITTER_CONSUMER_KEY, settings.TWITTER_CONSUMER_SECRET)
-        auth.set_access_token(sa.tokens['oauth_token'],sa.tokens['oauth_token_secret'])
+        auth = tweepy.OAuthHandler(settings.TWITTER_CONSUMER_KEY,
+                                   settings.TWITTER_CONSUMER_SECRET)
+        auth.set_access_token(sa.tokens['oauth_token'],
+                              sa.tokens['oauth_token_secret'])
         if options.get('save', True):
-           listener = RotatingFile(filename_prefix='sample',
-                   save_interval_seconds=options['interval'],
-                   data_dir=options['dir'])
-           stream = tweepy.Stream(auth, listener)
-           stream.sample()
+            listener = RotatingFile(
+                filename_prefix='sample',
+                save_interval_seconds=options['interval'],
+                data_dir=options['dir'])
+            stream = tweepy.Stream(auth, listener)
+            stream.sample()
         else:
-           listener = StdOutListener()
-           stream = tweepy.Stream(auth, listener)
-           StdOutListener(stream.sample())
+            listener = StdOutListener()
+            stream = tweepy.Stream(auth, listener)
+            StdOutListener(stream.sample())
