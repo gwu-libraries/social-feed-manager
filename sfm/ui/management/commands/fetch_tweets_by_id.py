d08923f9ec79d0cf9fe22140189edb4eee372fd4
kerchner <kerchner@gwu.edu>
2014-04-09 12:45:44 -0400

    Removed extraneous line

--- sfm/ui/management/commands/fetch_tweets_by_id.py
+++ sfm/ui/management/commands/fetch_tweets_by_id.py
@@ -27,7 +27,7 @@ class Command(BaseCommand):
 
         infile = options['inputfile']
         fin = open(infile, 'r+')
-        outstream = None
+        #outstream = None
         if options.get('outputfile', True):
             outfile = options['outputfile']
             outstream = open(outfile, "a")
