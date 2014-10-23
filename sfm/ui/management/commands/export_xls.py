ba1317696e92332956a772ace911ea143cd41655
kerchner <kerchner@gwu.edu>
2014-10-23 12:11:10 -0400

    Rewrote export_xls as a wrapper call to export_csv with --xls option

--- sfm/ui/management/commands/export_xls.py
+++ sfm/ui/management/commands/export_xls.py
@@ -1,12 +1,10 @@
-import codecs
-import sys
 from optparse import make_option
 
-from django.core.management import BaseCommand, call_command, CommandError
+from django.core.management import BaseCommand, call_command
 
 
 class Command(BaseCommand):
-    help = 'export data for a twitteruser or setname in excel'
+    help = 'export data for a twitteruser or setname in XLS format'
 
     option_list = BaseCommand.option_list + (
         make_option('--start-date', action='store', default=None,
@@ -21,38 +19,16 @@ class Command(BaseCommand):
         make_option('--set-name', action='store', default=None,
                     type='string', dest='set_name',
                     help='set name to export'),
+        make_option('--filename', action='store', default=None,
+                    type='string', dest='filename',
+                    help='filename to export'),
     )
 
-    @classmethod
-    def usage(self, *args):
-        usage = 'Usage: ./manage.py export_xls [filename] ' + \
-                '[options]' + '\n' + '\n' + self.help
-        return usage
-
     def handle(self, *args, **options):
-        if len(args) == 0:
-            raise CommandError("need a file name")
-        extra_option = {}
-        extra_option['xls'] = 'xls'
-        extra_option['f_name'] = args[0] + '.xls'
-        for opt in options:
-            if options[opt] is not None:
-                tuser = options['twitter_user']
-                start_dt = options['start_date']
-                end_dt = options['end_date']
-                st_name = options['set_name']
-                try:
-                    call_command('export_csv', extra_option,
-                                 twitter_user=tuser, start_date=start_dt,
-                                 end_date=end_dt, set_name=st_name,)
-                except Exception as e:
-                    if sys.stderr:
-                        print e
-                    else:
-                        try:
-                            writer_class = codecs.getwriter('utf-8')
-                            sys.stdout = writer_class(sys.stdout)
-                        except Exception as e:
-                            print e
-            else:
-                continue
+        """Calls export_csv, passing all options plus --xls."""
+        call_command('export_csv', start_date=options['start_date'],
+                     end_date=options['end_date'],
+                     twitter_user=options['twitter_user'],
+                     set_name=options['set_name'],
+                     filename=options['filename'],
+                     xls=True)
